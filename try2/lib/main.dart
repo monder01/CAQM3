d211f44dac793bbe07f314e91c9ea6f4885f1456
monder01 <m.arabob@uot.edu.ly>
2025-11-05 16:57:51 +0200

    now actully database works perfectly

--- try2/lib/main.dart
+++ try2/lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ClinicApp());
 }
 
--- try2/lib/models/admins.dart
+++ try2/lib/models/admins.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 
 import 'Users.dart';
 
