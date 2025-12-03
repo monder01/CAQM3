caeee46abe431a17a62549e60d82a26e127dfe7b
monder01 <m.arabob@uot.edu.ly>
2025-12-03 04:11:48 +0200

    ??

---
+++ MyProject/lib/pages/checkInAdmin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Checkinadmin extends StatefulWidget {
+  const Checkinadmin({super.key});
+
+  @override
+  State<Checkinadmin> createState() => _CheckinadminState();
+}
+
+class _CheckinadminState extends State<Checkinadmin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
