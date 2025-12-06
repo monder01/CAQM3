71c516f31b09143d963d1dfeb08e1638e8a32d1d
melad023 <meladzghira2001@gmail.com>
2025-12-07 00:31:47 +0200

    سجل مرضي أكثر واقعية

---
+++ MyProject/lib/FormManagementComponent/medicalHistoryPageB.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '/FormManagementComponent/forms.dart';
+import '/NotificationSystemComponent/notifications.dart';
+
+class Medicalhistorypageb extends StatefulWidget {
+  const Medicalhistorypageb({super.key});
+
+  @override
+  State<Medicalhistorypageb> createState() => _MedicalhistorypagebState();
+}
+
+class _MedicalhistorypagebState extends State<Medicalhistorypageb> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
