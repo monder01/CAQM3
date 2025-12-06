3d7fb497041fbab3113f7302ed3965bdb78a84a8
monder01 <m.arabob@uot.edu.ly>
2025-12-06 09:18:52 +0200

    adding try/catch,required for firebase methods

--- MyProject/lib/fireBase.dart
+++ MyProject/lib/fireBase.dart
@@ -9,42 +9,62 @@ class FirebaseService {
 
   //methods
   Future<UserCredential?> signIn(String email, String password) async {
-    userCredential = await auth.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    return userCredential;
+    try {
+      return await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      print("Sign-in Error: $e");
+      return null;
+    }
   }
 
-  Future<DocumentSnapshot?> getUserData(String uid) async {
-    documentSnapshot = await firestore.collection("users").doc(uid).get();
-    return documentSnapshot;
+  Future<DocumentSnapshot?> getUserData(String uid, String collection) async {
+    try {
+      return await firestore.collection(collection).doc(uid).get();
+    } catch (e) {
+      print("User Data Error: $e");
+      return null;
+    }
   }
 
-  Future<List<QueryDocumentSnapshot>> getSearchWithOne(
-    String searchedFor,
-    String collectionA,
-    String collectionB,
-  ) async {
-    final snapshot = await firestore
-        .collection(collectionA)
-        .where(collectionB, isEqualTo: searchedFor)
-        .get();
-    return snapshot.docs;
+  Future<List<QueryDocumentSnapshot>> getSearchWithOne({
+    required String searchedValue,
+    required String collection,
+    required String field,
+  }) async {
+    try {
+      final snapshot = await firestore
+          .collection(collection)
+          .where(field, isEqualTo: searchedValue)
+          .get();
+
+      return snapshot.docs;
+    } catch (e) {
+      print("Search Error: $e");
+      return [];
+    }
   }
 
-  Future<List<QueryDocumentSnapshot>> getSearchWithTwo(
-    String searchedForB,
-    String searchedForBB,
-    String collectionA,
-    String collectionB,
-    String collectiomBB,
-  ) async {
-    final snapshot = await firestore
-        .collection(collectionA)
-        .where(collectionB, isEqualTo: searchedForB)
-        .where(collectiomBB, isEqualTo: searchedForBB)
-        .get();
-    return snapshot.docs;
+  Future<List<QueryDocumentSnapshot>> getSearchWithTwo({
+    required String valueA,
+    required String valueB,
+    required String collection,
+    required String fieldA,
+    required String fieldB,
+  }) async {
+    try {
+      final snapshot = await firestore
+          .collection(collection)
+          .where(fieldA, isEqualTo: valueA)
+          .where(fieldB, isEqualTo: valueB)
+          .get();
+
+      return snapshot.docs;
+    } catch (e) {
+      print("Search Error: $e");
+      return [];
+    }
   }
 }
