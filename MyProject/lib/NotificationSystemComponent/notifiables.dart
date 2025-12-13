95d8a2d37d042076f5aada54b78028895155e143
monder01 <m.arabob@uot.edu.ly>
2025-12-13 22:53:53 +0200

    notify show by time test works

--- MyProject/lib/NotificationSystemComponent/notifiables.dart
+++ MyProject/lib/NotificationSystemComponent/notifiables.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:MyCAQM/NotificationSystemComponent/notifications.dart';
 import 'package:flutter/material.dart';
 
@@ -10,20 +12,53 @@ class Notifiable extends StatefulWidget {
 
 class _NotifiableState extends State<Notifiable> {
   Notifications notify = Notifications();
-  DateTime now = DateTime.now();
-  String date = "";
+  late Timer _timer;
   @override
+  void initState() {
+    super.initState();
+
+    // 2. استخدم Timer.periodic للتشغيل المتكرر
+    _timer = Timer.periodic(const Duration(seconds: 10), (timer) {
+      TimeOfDay timeNow = TimeOfDay.now();
+      DateTime dateNow = DateTime.now();
+      String formattedTime = timeNow.format(context);
+      String formattedDate = dateNow.toIso8601String().substring(0, 10);
+      String tryDate = '2025-12-13';
+      String tryTime = '10:46 PM';
+      if (formattedTime == tryTime && formattedDate == tryDate) {
+        print('Time: $formattedTime Date: $formattedDate');
+        notify.showConfirmationDialog(context, 'Date Matched!');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // 3. الأهم: تذكر إلغاء المؤقت عند إزالة الويدجت من الشجرة
+    _timer.cancel();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('صفحة الاشعارات'),
         backgroundColor: Colors.amberAccent[200],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [],
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                print("music playing");
+              },
+              child: Text('Play music'),
+            ),
+          ],
+        ),
       ),
     );
   }
