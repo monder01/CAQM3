d4524b3f5a4ca54f3eaf15657afd102e03a77918
melad023 <meladzghira2001@gmail.com>
2025-12-16 18:56:56 +0200

    تم

--- MyProject/lib/main.dart
+++ MyProject/lib/main.dart
@@ -2,22 +2,11 @@ import 'package:MyCAQM/UserManagementComponent/signin.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-final FlutterLocalNotificationsPlugin notificationsPlugin =
-    FlutterLocalNotificationsPlugin(); // إنشاء مثيل لإدارة الإشعارات المحلية
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // التأكد من تهيئة ربط فلاتر قبل تنفيذ أي عمليات غير متزامنة
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // تهيئة خدمات فايربيز باستخدام الإعدادات الخاصة بالمنصة الحالية
-  // تهيئة الإشعارات
-  const AndroidInitializationSettings androidSettings =
-      AndroidInitializationSettings('@mipmap/ic_launcher');
-  const InitializationSettings settings = InitializationSettings(
-    android: androidSettings,
-  );
-  await notificationsPlugin.initialize(settings);
-
   runApp(MyApp()); // تشغيل التطبيق وإرسال الواجهة الأساسية إليه
 }
 
