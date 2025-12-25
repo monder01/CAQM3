08fd8fa7e928cc9b3005709bd204f6ef884749e6
monder01 <m.arabob@uot.edu.ly>
2025-12-26 00:26:05 +0200

    editing factory design

--- MyProject/lib/AppointmentManagementComponent/appointment_factory.dart
+++ MyProject/lib/AppointmentManagementComponent/appointment_factory.dart
@@ -1,14 +1,35 @@
-class AppointmentFactory {
+import 'package:MyCAQM/AppointmentManagementComponent/appointments.dart';
+
+class AppointmentFactory extends Appointment {
   static double getCost(String type) {
     switch (type) {
       case 'إستشارة':
         return 30.0;
       case 'متابعة':
-        return 70.0;
+        return 40.0;
       case 'فحص دوري':
         return 50.0;
+      case 'تحليل':
+        return 100.0;
+      case 'جراحة':
+        return 150.0;
+      case 'علاج طبيعي':
+        return 200.0;
+
       default:
         return 0.0;
     }
   }
 }
+
+class Consultation extends Appointment {}
+
+class FollowUp extends Appointment {}
+
+class RoutineCheckup extends Appointment {}
+
+class LabTest extends Appointment {}
+
+class Surgery extends Appointment {}
+
+class NaturalHealing extends Appointment {}
